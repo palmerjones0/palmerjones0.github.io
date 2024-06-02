884c7c3ef16f1679ee592adcc4492d1ba407784a
palmerjones0 <52291135+palmerjones0@users.noreply.github.com>
2024-06-02 12:38:53 -0400

    Update init.js

--- assets/js/viewstl/init.js
+++ assets/js/viewstl/init.js
@@ -7,9 +7,9 @@ function initStlViewer() {
         console.log('Initing 3D File: ' + filePath);
         //create new viewer
         new StlViewer(elem, { models: [{ filename: filePath,
-                                         rotationx: -3.14/4,
+                                         rotationx: -3.14/2,
                                          rotationy: 0,
-                                         rotationz: -3.14/4, }],
+                                         rotationz: -3.14/2, }],
                              auto_rotate:true });
     });
 }
